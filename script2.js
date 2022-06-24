c54935e0c45c7fbd29ee560cab2f74038f8ad01e
Gokulcasper <ronitcasper215@gmail.com>
2022-06-24 17:59:27 +0530

    Feat : setting & Getting

--- script2.js
+++ script2.js
@@ -2,30 +2,30 @@ const nameEl = document.querySelector("#input-delName input");
 
 // // Setting the attribute :
 // // Element.setAttribute("name", "FullName")// set the attribete with value
-// nameEl.setAttribute("value", "Gokul");
+nameEl.setAttribute("value", "Gokul");
 
 // // Getting the attribute :
-// const isAliveDom = nameEl.getAttribute("isAlive");
+const isAliveDom = nameEl.getAttribute("isAlive");
 
-// const placeDom = nameEl.getAttribute("place");
+const placeDom = nameEl.getAttribute("place");
 
-// const interestDom = nameEl.getAttribute("interest");
+const interestDom = nameEl.getAttribute("interest");
 
-// console.log(`${isAliveDom}
-// ${placeDom}
-// ${interestDom}`);
+console.log(`${isAliveDom}
+${placeDom}
+${interestDom}`);
 
-// console.log(nameEl.attributes); // List all Attributes in Elements
-// console.log(nameEl.attributes[6].value); // Attributes Index and its VAlue
-// console.log(nameEl.attributes.place.value); // Attributes By Property Name
-// console.log(nameEl.attributes["interest"].value); // Attributes By Bracket Notation
+console.log(nameEl.attributes); // List all Attributes in Elements
+console.log(nameEl.attributes[6].value); // Attributes Index and its VAlue
+console.log(nameEl.attributes.place.value); // Attributes By Property Name
+console.log(nameEl.attributes["interest"].value); // Attributes By Bracket Notation
 
 // // Methods :
-// console.log(nameEl.hasAttribute("place")); // Check Attribute
+console.log(nameEl.hasAttribute("place")); // Check Attribute
 
-// nameEl.removeAttribute("isAlive"); // Remove Attribute
+nameEl.removeAttribute("isAlive"); // Remove Attribute
 
-// console.log(nameEl.style); // Return CSS Style Declaration
+console.log(nameEl.style); // Return CSS Style Declaration
 
 console.log(nameEl.dataset); //Attributes  Value in Dataset
 console.log(nameEl.dataset.place); //get value of attributes in dataset
